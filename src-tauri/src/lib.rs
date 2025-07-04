465dafa2f0b9ec8d744f9e1c99e3c18551417b4c
andeya <andeyalee@outlook.com>
2025-07-05 02:39:31 +0800

    feat: add log

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,21 +1,18 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-use std::time::{SystemTime, UNIX_EPOCH};
-
-#[tauri::command]
-fn greet() -> String {
-  let now = SystemTime::now();
-  let epoch_ms = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
-  format!("Current timestamp from Rust: {epoch_ms}ms")
-}
+use std::{
+  env,
+  time::{SystemTime, UNIX_EPOCH},
+};
+#[allow(unused_imports)]
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .setup(|_app| {
-      std::thread::spawn(|| {
-        tokio::runtime::Runtime::new()
-          .expect("error while creating runtime")
-          .block_on(init());
+    .setup(|app| {
+      let handle = app.handle().clone();
+      tauri::async_runtime::block_on(async move {
+        setup(&handle).await;
       });
       Ok(())
     })
@@ -25,6 +22,23 @@ pub fn run() {
     .expect("error while running tauri application");
 }
 
-async fn init() {
-  // println!("init...");
+#[allow(unused_variables)]
+async fn setup<R: tauri::Runtime>(app: &tauri::AppHandle<R>) {
+  init_log();
+  #[cfg(debug_assertions)]
+  {
+    let app_data_dir = app.app_handle().path().app_data_dir().unwrap();
+    log::debug!("app_data_dir={}", app_data_dir.display());
+  }
+}
+
+fn init_log() {
+  env_logger::try_init().expect("error initializing log");
+}
+
+#[tauri::command]
+fn greet() -> String {
+  let now = SystemTime::now();
+  let epoch_ms = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
+  format!("Current timestamp from Rust: {epoch_ms}ms")
 }
