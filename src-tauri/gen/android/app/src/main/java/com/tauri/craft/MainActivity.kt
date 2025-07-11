22923d1e3f18fc0c41855c988db67df1b9ba15ba
andeya <andeyalee@outlook.com>
2025-07-11 19:36:57 +0800

    Tauri + Quasar + TS + Vite

---
+++ src-tauri/gen/android/app/src/main/java/com/tauri/craft/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.tauri.craft
+
+class MainActivity : TauriActivity()
---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,42 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+#[allow(unused_imports)]
+use tauri::Manager;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+  tauri::Builder::default()
+    .setup(|app| {
+      let handle = app.handle().clone();
+      tauri::async_runtime::block_on(async move {
+        setup(&handle).await;
+      });
+      Ok(())
+    })
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![time_now])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+#[allow(unused_variables)]
+async fn setup<R: tauri::Runtime>(app: &tauri::AppHandle<R>) {
+  init_log();
+  #[cfg(debug_assertions)]
+  {
+    app.get_webview_window("main").unwrap().open_devtools();
+    let app_data_dir = app.app_handle().path().app_data_dir().unwrap();
+    log::debug!("app_data_dir={}", app_data_dir.display());
+  }
+}
+
+fn init_log() {
+  env_logger::try_init().expect("error initializing log");
+}
+
+#[tauri::command]
+fn time_now() -> String {
+  let now = chrono::Local::now();
+  let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
+  format!("Rust Time: {formatted_time}")
+}
