c87d210e8c8accbe317007cbf25e53b134254c63
andeya <andeyalee@outlook.com>
2025-08-18 16:08:53 +0800

    fix: bug

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,10 +12,10 @@ use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  let builder = tauri::Builder::default();
+  let mut builder = tauri::Builder::default();
   #[cfg(desktop)]
   {
-    builder = builder.plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+    builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
       let _ = app
         .get_webview_window("main")
         .expect("no main window")
